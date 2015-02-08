f1a9579926967366e8df863ed9b8434356e7c37d
Huy Le <huyle333@yahoo.com>
2015-02-07 21:55:17 -0500

    added chill html and css

---
+++ html/js/chill.js
@@ -0,0 +1,22 @@
+/* jQuery to listen too the size of window and adjust accordingly
+$(function() {
+    $(window).on('resize', function resize()  {
+        $(window).off('resize', resize);
+        setTimeout(function () {
+            var content = $('#content');
+            var top = (window.innerHeight - content.height()) / 2;
+            content.css('top', Math.max(0, top) + 'px');
+            $(window).on('resize', resize);
+        }, 50);
+    }).resize();
+});
+*/
+
+var count = 30;
+
+var counter = setInterval(timer, 1000);
+
+$(function timer(){
+
+
+});
